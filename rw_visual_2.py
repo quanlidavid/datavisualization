af131c7536ccf09f1f44c42729c727a07074ec29
liquan <liquan@wwwfile.cn>
2022-05-13 19:21:42 +0800

    update

--- rw_visual_2.py
+++ rw_visual_2.py
@@ -1,4 +1,5 @@
 import plotly.graph_objs as go
+import numpy as np
 
 from random_walk import RandomWalk
 
@@ -6,3 +7,21 @@ while True:
     rw = RandomWalk(5_000)
     rw.fill_walk()
 
+    fig = go.Figure(data=go.Scatter(
+        x=rw.x_values,
+        y=rw.y_values,
+        mode='markers',
+        name='Random Walk',
+        marker=dict(
+            color=np.arange(rw.num_points),
+            size=8,
+            colorscale='Greens',
+            showscale=True
+        )
+    ))
+
+    fig.show()
+
+    keep_running = input('Make another walk?(y/n): ')
+    if keep_running == 'n':
+        break
